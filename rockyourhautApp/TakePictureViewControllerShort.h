70b6e3bb2244e4b27502b3d6b241200f5b02d99b
Duesselentchen <bettina@ebert-wolfgang.de>
2013-07-13 22:47:45 +0200

    Titlepage & second ImageView
    Nice Title and copyCat

---
+++ rockyourhautApp/TakePictureViewControllerShort.h
@@ -0,0 +1,13 @@
+//
+//  TakePictureViewControllerShort.h
+//  rockyourhautApp
+//
+//  Created by Bettina Ebert on 13.07.13.
+//  Copyright (c) 2013 Korbinian Breu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TakePictureViewControllerShort : UIViewController
+
+@end
---
+++ rockyourhautApp/TakePictureViewControllerShort.m
@@ -0,0 +1,38 @@
+//
+//  TakePictureViewControllerShort.m
+//  rockyourhautApp
+//
+//  Created by Bettina Ebert on 13.07.13.
+//  Copyright (c) 2013 Korbinian Breu. All rights reserved.
+//
+
+#import "TakePictureViewControllerShort.h"
+
+@interface TakePictureViewControllerShort ()
+
+@end
+
+@implementation TakePictureViewControllerShort
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
